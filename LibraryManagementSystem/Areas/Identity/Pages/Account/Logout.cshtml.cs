50b2b0fa412d211e67bbc623395b5fe54e8d7b82
Software Group <softwaregroup470@gmail.com>
2025-07-15 01:11:39 +0600

    Committed

--- LibraryManagementSystem/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ LibraryManagementSystem/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -1,9 +1,8 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-#nullable disable
-
-using System;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using LibraryManagementSystem.Controllers;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -12,6 +11,7 @@ using Microsoft.Extensions.Logging;
 
 namespace LibraryManagementSystem.Areas.Identity.Pages.Account
 {
+    [AllowAnonymous]
     public class LogoutModel : PageModel
     {
         private readonly SignInManager<IdentityUser> _signInManager;
@@ -21,6 +21,10 @@ namespace LibraryManagementSystem.Areas.Identity.Pages.Account
         {
             _signInManager = signInManager;
             _logger = logger;
+        }
+
+        public void OnGet()
+        {
         }
 
         public async Task<IActionResult> OnPost(string returnUrl = null)
@@ -33,9 +37,7 @@ namespace LibraryManagementSystem.Areas.Identity.Pages.Account
             }
             else
             {
-                // This needs to be a redirect so that the browser performs a new
-                // request and the identity for the user gets updated.
-                return RedirectToPage();
+                return RedirectToPage("Index" , "Home");
             }
         }
     }
---
+++ LibraryManagementSystem/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -0,0 +1,92 @@
+﻿///Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LibraryManagementSystem.Areas.Identity.Pages.Account.Manage
+{
+    public partial class IndexModel : PageModel
+    {
+        public UserManager<IdentityUser> UserManager { get; } // ✅ Now it's public and has a getter
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public IndexModel(
+            UserManager<IdentityUser> userManager,
+            SignInManager<IdentityUser> signInManager)
+        {
+            UserManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public string Username { get; set; }
+
+        [TempData]
+        public string StatusMessage { get; set; }
+
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public class InputModel
+        {
+            [Phone]
+            [Display(Name = "Phone number")]
+            public string PhoneNumber { get; set; }
+        }
+
+        private async Task LoadAsync(IdentityUser user)
+        {
+            var userName = await UserManager.GetUserNameAsync(user);
+            var phoneNumber = await UserManager.GetPhoneNumberAsync(user);
+
+            Username = userName;
+
+            Input = new InputModel
+            {
+                PhoneNumber = phoneNumber
+            };
+        }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            var user = await UserManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return NotFound($"Unable to load user.");
+            }
+
+            await LoadAsync(user);
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            var user = await UserManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return NotFound($"Unable to load user.");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                await LoadAsync(user);
+                return Page();
+            }
+
+            var phoneNumber = await UserManager.GetPhoneNumberAsync(user);
+            if (Input.PhoneNumber != phoneNumber)
+            {
+                var setPhoneResult = await UserManager.SetPhoneNumberAsync(user, Input.PhoneNumber);
+                if (!setPhoneResult.Succeeded)
+                {
+                    StatusMessage = "Unexpected error setting phone number.";
+                    return RedirectToPage();
+                }
+            }
+
+            await _signInManager.RefreshSignInAsync(user);
+            StatusMessage = "Profile updated successfully.";
+            return RedirectToPage();
+        }
+    }
+}
